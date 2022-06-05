177a89dad4c776e0a0f47b129c0558db55a4566f
iHarryD <anuraagvr555@gmail.com>
2022-06-06 01:23:36 +0530

    feat: add create post component

--- interfaces/LoggedOutHomeSidebar.interface.ts
+++ interfaces/LoggedOutHomeSidebar.interface.ts
@@ -1,7 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
 import { loginSetter, toLoginSetter } from "./Common.type";
 
 export interface loggedOutSidebarProps {
-  toLoginSetter: toLoginSetter;
   loginSetter: loginSetter;
 }
