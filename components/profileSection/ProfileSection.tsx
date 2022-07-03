c6ffdc345adb1f813470675e434dff80cc8f6b2c
iHarryD <anuraagvr555@gmail.com>
2022-07-03 16:55:36 +0530

    refactor: switch to client side fetching in profil

--- components/profileSection/ProfileSection.tsx
+++ components/profileSection/ProfileSection.tsx
@@ -3,27 +3,49 @@ import buttonsStyles from "../../styles/Buttons.module.css";
 import commonStyles from "../../styles/Common.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareNodes } from "@fortawesome/free-solid-svg-icons";
-import { useRef } from "react";
-import { User, UserWithStats } from "../../interfaces/Common.interface";
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { UserWithStats } from "../../interfaces/Common.interface";
 
-export default function ProfileSection({
-  user,
-  isAdmin,
-}: {
-  user: UserWithStats;
-  isAdmin: boolean;
-}) {
+export default function ProfileSection() {
+  const {
+    query: { user: userQuery },
+  } = useRouter();
+  const { data: session } = useSession();
+  const [user, setUser] = useState<UserWithStats | null>(null);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
   const nameInputRef = useRef<HTMLInputElement | null>(null);
   const bioInputRef = useRef<HTMLTextAreaElement | null>(null);
   // const imageInputRef = useRef<HTMLInputElement>(null)
 
-  async function handleUpdateUser({
-    name,
-    bio,
-  }: {
-    name: string;
-    bio: string;
-  }) {
+  useEffect(() => {
+    if (userQuery) {
+      (async () => {
+        try {
+          const result = await fetch(
+            `http://127.0.0.1:3000/api/profile?user=${userQuery}`
+          );
+          const data = await JSON.parse(JSON.stringify(result));
+          setUser(data);
+          if (session?.user.username === userQuery) {
+            setIsAdmin(true);
+          } else {
+            setIsAdmin(false);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      })();
+    } else {
+      if (session) {
+        setUser(session.user);
+        setIsAdmin(true);
+      }
+    }
+  }, [userQuery, session]);
+
+  async function handleUpdateUser(name: string, bio: string) {
     const data = {
       name,
       bio,
@@ -37,81 +59,89 @@ export default function ProfileSection({
     });
   }
   return (
-    <section className={profileSectionStyles.profileSection}>
-      <div className={profileSectionStyles.profileSectionUpper}>
-        <div className={profileSectionStyles.profilePictureUsernameContainer}>
-          <div>
-            <img
-              src={user.image}
-              alt="profile-picture"
-              className={profileSectionStyles.profilePicture}
-            />
-          </div>
-          <p
-            className={`${profileSectionStyles.username} ${commonStyles.username}`}
-          >
-            {user.username}
-          </p>
-        </div>
-        <div className={profileSectionStyles.nameBioInputContainer}>
-          <div
-            className={`${profileSectionStyles.profileUpdatingInputContainer} ${
-              isAdmin ? "" : profileSectionStyles.notAdmin
-            }`}
-          >
-            <label htmlFor="name">Name</label>
-            <input
-              ref={nameInputRef}
-              defaultValue={user.name}
-              id="name"
-              placeholder={user.name}
-              className={profileSectionStyles.profileUpdatingInput}
-            />
-          </div>
-          <div
-            className={`${profileSectionStyles.profileUpdatingInputContainer} ${
-              isAdmin ? "" : profileSectionStyles.notAdmin
-            }`}
-          >
-            <label htmlFor="bio">Bio</label>
-            <textarea
-              ref={bioInputRef}
-              defaultValue={user.bio}
-              id="bio"
-              placeholder={user.bio}
-              className={profileSectionStyles.profileUpdatingInput}
-            />
-          </div>
-        </div>
-        <div className={profileSectionStyles.profileSectionButtonContainer}>
-          <button className={profileSectionStyles.shareProfileButton}>
-            <FontAwesomeIcon icon={faShareNodes} />
-          </button>
-          {isAdmin && (
-            <button
-              className={buttonsStyles.primaryButton}
-              onClick={() => {
-                if (
-                  nameInputRef.current === null ||
-                  bioInputRef.current === null
-                )
-                  return;
-                handleUpdateUser({
-                  name: nameInputRef.current.value,
-                  bio: bioInputRef.current.value,
-                });
-              }}
+    <>
+      {user ? (
+        <section className={profileSectionStyles.profileSection}>
+          <div className={profileSectionStyles.profileSectionUpper}>
+            <div
+              className={profileSectionStyles.profilePictureUsernameContainer}
             >
-              Update
-            </button>
-          )}
-        </div>
-      </div>
-      <div className={profileSectionStyles.userStatsContainer}>
-        <p>{user.badges.length} badges</p>
-        <p>{user.connections.length} connections</p>
-        <p>{user.posts.length} posts</p>
-      </div>
-    </section>
+              <div>
+                <img
+                  src={user.image}
+                  alt="profile-picture"
+                  className={profileSectionStyles.profilePicture}
+                />
+              </div>
+              <p
+                className={`${profileSectionStyles.username} ${commonStyles.username}`}
+              >
+                {user.username}
+              </p>
+            </div>
+            <div className={profileSectionStyles.nameBioInputContainer}>
+              <div
+                className={`${
+                  profileSectionStyles.profileUpdatingInputContainer
+                } ${isAdmin ? "" : profileSectionStyles.notAdmin}`}
+              >
+                <label htmlFor="name">Name</label>
+                <input
+                  ref={nameInputRef}
+                  defaultValue={user.name}
+                  id="name"
+                  placeholder={user.name}
+                  className={profileSectionStyles.profileUpdatingInput}
+                />
+              </div>
+              <div
+                className={`${
+                  profileSectionStyles.profileUpdatingInputContainer
+                } ${isAdmin ? "" : profileSectionStyles.notAdmin}`}
+              >
+                <label htmlFor="bio">Bio</label>
+                <textarea
+                  ref={bioInputRef}
+                  defaultValue={user.bio}
+                  id="bio"
+                  placeholder={user.bio}
+                  className={profileSectionStyles.profileUpdatingInput}
+                />
+              </div>
+            </div>
+            <div className={profileSectionStyles.profileSectionButtonContainer}>
+              <button className={profileSectionStyles.shareProfileButton}>
+                <FontAwesomeIcon icon={faShareNodes} />
+              </button>
+              {isAdmin && (
+                <button
+                  className={buttonsStyles.primaryButton}
+                  onClick={() => {
+                    if (
+                      nameInputRef.current === null ||
+                      bioInputRef.current === null
+                    )
+                      return;
+                    handleUpdateUser(
+                      nameInputRef.current.value,
+                      bioInputRef.current.value
+                    );
+                  }}
+                >
+                  Update
+                </button>
+              )}
+            </div>
+          </div>
+          <div className={profileSectionStyles.userStatsContainer}>
+            <p>{user.badges.length} badges</p>
+            <p>{user.connections.length} connections</p>
+            <p>{user.posts.length} posts</p>
+          </div>
+        </section>
+      ) : (
+        "Loading..."
+      )}
+    </>
   );
 }
--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -1,25 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectToMongoDB from "../../lib/mongodb";
-import { ObjectId } from "mongodb";
-import { getSession } from "next-auth/react";
+import { MongoClient } from "mongodb";
 import { unstable_getServerSession } from "next-auth";
 import { nextAuthConfig } from "./auth/[...nextauth]";
+import connectToMongoDb from "../../lib/mongodb";
+import { cursorToDoc } from "../../helpers/cursorToDoc";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const session = await unstable_getServerSession(req, res, nextAuthConfig);
-  if (session === null) return;
-  const username = session.user.username;
+  const connectionPromise: { connection: null | MongoClient } = {
+    connection: null,
+  };
   const { method } = req;
   switch (method) {
+    case "GET":
+      try {
+        const { user } = req.query;
+        const username =
+          user ||
+          (await unstable_getServerSession(req, res, nextAuthConfig))?.user
+            .username;
+        if (!username) throw new Error();
+        connectionPromise.connection = await (await connectToMongoDb).connect();
+        const userDoc = await connectionPromise.connection
+          .db()
+          .collection("users")
+          .findOne({ username });
+        if (userDoc === null) throw new Error();
+        const badges = connectionPromise.connection
+          .db()
+          .collection("badges")
+          .find({ givenTo: username });
+        const connections = connectionPromise.connection
+          .db()
+          .collection("connections")
+          .find({ connectionBetween: { $in: [username] } });
+        const posts = connectionPromise.connection
+          .db()
+          .collection("posts")
+          .find({ postedBy: { username: username } });
+        const userDetails = {
+          badges: await cursorToDoc(badges),
+          connections: await cursorToDoc(connections),
+          posts: await cursorToDoc(posts),
+          ...(await cursorToDoc(userDoc)),
+        };
+        return res
+          .status(200)
+          .json({ message: "User fetched.", data: userDetails });
+      } catch (err) {
+        return res.status(500).send(err);
+      } finally {
+        if (connectionPromise.connection) {
+          connectionPromise.connection.close();
+        }
+      }
     case "PATCH":
-      const connection = await (await connectToMongoDB).connect();
+      const session = await unstable_getServerSession(req, res, nextAuthConfig);
+      if (session === null) return;
       const { name, bio } = req.body;
       try {
-        const updatedUser = await connection
+        connectionPromise.connection = await (await connectToMongoDB).connect();
+        const updatedUser = await connectionPromise.connection
           .db()
           .collection("users")
           .findOneAndUpdate(
-            { username },
+            { username: session.user.username },
             {
               $set: {
                 name: name,
@@ -32,7 +77,9 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       } catch (err) {
         return res.status(500).send(err);
       } finally {
-        connection.close();
+        if (connectionPromise.connection) {
+          connectionPromise.connection.close();
+        }
       }
     default:
       return res
