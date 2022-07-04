a341a8b0b2f6e0c623fc8fbdd47e3839b8a7183e
iHarryD <anuraagvr555@gmail.com>
2022-07-04 13:23:40 +0530

    feat: add post interface

--- interfaces/Common.interface.ts
+++ interfaces/Common.interface.ts
@@ -12,3 +12,25 @@ export interface UserWithStats extends User {
   connections: string[];
   posts: string[];
 }
+
+export interface PostComment {
+  postedBy: {
+    name: string;
+    username: string;
+  };
+  comment: string;
+  timestamp: Date;
+}
+
+export interface Post {
+  _id: string;
+  caption: string;
+  comments: PostComment[];
+  likes: { username: string; name: string }[];
+  media: string[];
+  postedBy: {
+    name: string;
+    username: string;
+  };
+  timestamp: string;
+}
