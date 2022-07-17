cf51ce4d3f009dab0b508f2caf184564f80d478e
iHarryD <anuraagvr555@gmail.com>
2022-07-17 15:51:47 +0530

    feat: add chat interfaces

--- interfaces/Common.interface.ts
+++ interfaces/Common.interface.ts
@@ -1,3 +1,9 @@
+enum ChatMessageStatus {
+  SEDNING = "sending",
+  SENT = "sent",
+  SEEN = "seen",
+}
+
 export interface User {
   bio: string;
   email: string;
@@ -33,4 +39,28 @@ export interface Post {
     username: string;
   };
   timestamp: string;
+}
+
+export interface ChatMessage {
+  _id: string;
+  message: string;
+  sender: string;
+  status: ChatMessageStatus;
+  receiver: string;
+  timestamp: string;
+}
+
+export interface Chat {
+  _id: string;
+  chatBetween: { image: string; name: string; username: string }[];
+  conversation: ChatMessage[];
+  timestamp: string;
+}
+
+export interface ChatPreview {
+  _id: string;
+  avatar: string;
+  name: string;
+  lastMessage: string;
+  lastMessageTime: string;
 }
