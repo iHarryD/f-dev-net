691fb239385539fcfdf8efe095e16426fbcbe627
iHarryD <anuraagvr555@gmail.com>
2022-07-20 10:59:15 +0530

    feat: add more actions in post reducer

--- features/postSlice.ts
+++ features/postSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Post } from "../interfaces/Common.interface";
+import { Post, PostComment } from "../interfaces/Common.interface";
 import { PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: Post[] = [];
@@ -17,9 +17,39 @@ export const postSlice = createSlice({
     reset(state) {
       return [];
     },
+    like(state, action: PayloadAction<{ postID: string; username: string }>) {
+      const indexOfPost = state.findIndex(
+        (post) => post._id === action.payload.postID
+      );
+      if (indexOfPost >= 0) {
+        state[indexOfPost].likes.push(action.payload.username);
+      }
+    },
+    unlike(state, action: PayloadAction<{ postID: string; username: string }>) {
+      const indexOfPost = state.findIndex(
+        (post) => post._id === action.payload.postID
+      );
+      if (indexOfPost >= 0) {
+        state[indexOfPost].likes = state[indexOfPost].likes.filter(
+          (likeBy) => likeBy !== action.payload.username
+        );
+      }
+    },
+    updateComments(
+      state,
+      action: PayloadAction<{ postID: string; comments: PostComment[] }>
+    ) {
+      const indexOfPost = state.findIndex(
+        (post) => post._id === action.payload.postID
+      );
+      if (indexOfPost >= 0) {
+        state[indexOfPost].comments = action.payload.comments;
+      }
+    },
   },
 });
 
-export const { append, refresh, reset } = postSlice.actions;
+export const { append, refresh, reset, like, unlike, updateComments } =
+  postSlice.actions;
 
 export default postSlice.reducer;
