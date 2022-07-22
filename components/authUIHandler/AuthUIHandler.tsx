f8483564eba2527fa17f27b133925ae3a2ac87c9
iHarryD <anuraagvr555@gmail.com>
2022-07-23 00:31:49 +0530

    refactor: change type of children in auth handler

--- components/authUIHandler/AuthUIHandler.tsx
+++ components/authUIHandler/AuthUIHandler.tsx
@@ -1,8 +1,7 @@
-import { ReactNode } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import InitialFullPageLoader from "../initialFullPageLoader/InitialFullPageLoader";
 
-export default function AuthUIHandler({ children }: { children: ReactNode }) {
+export default function AuthUIHandler({ children }: { children: JSX.Element }) {
   const { status } = useAuth();
   return status === "loading" ? <InitialFullPageLoader /> : children;
 }
