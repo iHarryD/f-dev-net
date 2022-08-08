84b5629cdd736dc07204372a24c563077ab18508
iHarryD <anuraagvr555@gmail.com>
2022-08-08 11:39:41 +0530

    feat: update localStorage user data when loads

--- components/authUIHandler/AuthUIHandler.tsx
+++ components/authUIHandler/AuthUIHandler.tsx
@@ -1,18 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "../../features/userSlice";
+import { login, logout, updateUser } from "../../features/userSlice";
 import { UserAuthStatus } from "../../interfaces/Common.interface";
-import { RootState } from "../../store";
+import { AppDispatch, RootState } from "../../store";
 import InitialFullPageLoader from "../initialFullPageLoader/InitialFullPageLoader";
 
 export default function AuthUIHandler({ children }: { children: JSX.Element }) {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   const { status } = useSelector((state: RootState) => state.userSlice);
   useEffect(() => {
     const user = localStorage.getItem("user");
     const token = localStorage.getItem("token");
     if (user && token) {
       dispatch(login({ user: JSON.parse(user), token: token }));
+      dispatch(updateUser());
     } else {
       dispatch(logout());
     }
